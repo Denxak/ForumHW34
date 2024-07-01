e31a7bb06caf50a715eae9ef96c79524433e41e9
Denxak <denxak@gmail.com>
2024-07-01 23:58:22 +0200

    Update ForumImpl.java

--- ForumHW34/src/forum/dao/ForumImpl.java
+++ ForumHW34/src/forum/dao/ForumImpl.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.function.Predicate;
 
 public class ForumImpl implements Forum {
     private Post[] posts;
@@ -25,12 +24,11 @@ public class ForumImpl implements Forum {
         if (post == null || getPostById(post.getPostId()) != null) {
             return false;
         }
-        Post[] postsCopy = Arrays.copyOf(posts, posts.length + 1);
+        posts = Arrays.copyOf(posts, size + 1);
         int index = Arrays.binarySearch(posts, 0, size, post, comparator);
         index = index >= 0 ? index : -index - 1;
-        System.arraycopy(posts, index, postsCopy, index + 1, size - index);
-        postsCopy[index] = post;
-        posts = postsCopy;
+        System.arraycopy(posts, index, posts, index + 1, size - index);
+        posts[index] = post;
         size++;
         return true;
     }
@@ -39,10 +37,8 @@ public class ForumImpl implements Forum {
     public boolean removePost(int postId) {
         for (int i = 0; i < size; i++) {
             if (posts[i].getPostId() == postId) {
-                Post[] postsCopy = Arrays.copyOf(posts, posts.length - 1);
-                System.arraycopy(posts, i + 1, postsCopy, i, size - i - 1);
-                posts = postsCopy;
-                size--;
+                System.arraycopy(posts, i + 1, posts, i, size - i - 1);
+                posts = Arrays.copyOf(posts, --size);
                 return true;
             }
         }
@@ -71,7 +67,7 @@ public class ForumImpl implements Forum {
 
     @Override
     public Post[] getPostsByAuthor(String author) {
-        return findPostsByPredicate(p -> p.getAuthor().equals(author));
+        return getPostsByAuthor(author, LocalDate.MIN, LocalDate.MAX);
     }
 
     @Override
@@ -80,26 +76,17 @@ public class ForumImpl implements Forum {
         pattern.setDate(dateFrom.atStartOfDay());
         int from = Arrays.binarySearch(posts, 0, size, pattern, comparator);
         from = from >= 0 ? from : -from - 1;
+
         pattern = new Post(0, null, author, null);
         pattern.setDate(dateTo.atTime(LocalTime.MAX));
         int to = Arrays.binarySearch(posts, 0, size, pattern, comparator);
         to = to >= 0 ? to : -to - 1;
+
         return Arrays.copyOfRange(posts, from, to);
     }
 
     @Override
     public int size() {
         return size;
-    }
-
-    private Post[] findPostsByPredicate(Predicate<Post> predicate) {
-        Post[] res = new Post[size];
-        int j = 0;
-        for (int i = 0; i < size; i++) {
-            if (predicate.test(posts[i])) {
-                res[j++] = posts[i];
-            }
-        }
-        return Arrays.copyOf(res, j);
     }
 }
